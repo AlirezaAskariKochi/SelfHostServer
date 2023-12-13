b08b0c7db11bb8dc451e720f135c83cdb6621d53
alireza askari <Alireza_Askari_Kochi@outlook.com>
2023-12-13 20:50:36 +0330

    int

---
+++ src/SelfHostServer.Host/Controllers/OrderController.cs
@@ -0,0 +1,26 @@
+﻿using SelfHostServer.Host.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace SelfHostServer.Host.Controllers
+{
+    public class OrderController : ApiController
+    {
+        Order[] Orders = new Order[]
+        {
+            new Order { Id = 1,Name= "Tomato soup" , Category="Groceries", Price=10},
+            new Order { Id = 2,Name= "Yo yo" , Category="Toys", Price=2.67M},
+            new Order { Id = 3,Name= "HDD" , Category="Hardware", Price=20.88M},
+            new Order { Id = 4,Name= "StrongHold2" , Category="Game", Price=11.25M},
+        };
+
+        public IEnumerable<Order> GetAllOrders()
+        {
+            return Orders;
+        }
+    }
+}
---
+++ src/SelfHostServer.Host/Controllers/ProductController.cs
@@ -0,0 +1,27 @@
+﻿using SelfHostServer.Host.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace SelfHostServer.Host.Controllers
+{
+    public class ProductController : ApiController
+    {
+        Product[] products = new Product[]
+        {
+            new Product{Id=1, Name="Product 1"},
+            new Product{Id=2, Name="Product 2"},
+            new Product{Id=3, Name="Product 3"},
+            new Product{Id=4, Name="Product 4"}
+        };
+
+        [HttpGet]
+        public IEnumerable<Product> All()
+        {
+            return products;
+        }
+    }
+}
